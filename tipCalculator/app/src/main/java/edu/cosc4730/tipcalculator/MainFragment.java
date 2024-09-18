bd4cd631c8f03812a6048d8019901ba9f53191e8
Alex Bryan <abryan9@uwyo.edu>
2024-09-18 11:25:09 -0600

    Layouts and listeners.

--- tipCalculator/app/src/main/java/edu/cosc4730/tipcalculator/MainFragment.java
+++ tipCalculator/app/src/main/java/edu/cosc4730/tipcalculator/MainFragment.java
@@ -1,23 +1,74 @@
 package edu.cosc4730.tipcalculator;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import edu.cosc4730.tipcalculator.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
-    private FragmentMainBinding binding;
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState
     ) {
-        binding = FragmentMainBinding.inflate(inflater, container, true);
+        edu.cosc4730.tipcalculator.databinding.FragmentMainBinding binding = FragmentMainBinding.inflate(inflater, container, true);
+        binding.billEnter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        binding.tipEnter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        binding.tenPercent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        binding.fifteenPercent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        binding.tipCustom.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+
+        // add switch bar for rounding
         return binding.getRoot();
     }
 }
